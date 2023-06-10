b6e04fde045dfff4344bef3e8aa8659b096adbd0
Yufei Wang <y3422wan@uwaterloo.ca>
2023-06-10 19:30:21 +0800

    api 文档更改

--- _Backend/BackEnd_py/app.py
+++ _Backend/BackEnd_py/app.py
@@ -30,50 +30,57 @@ class Hello(Resource):
 
 llm_conversation = []
 
-class LLM_Access_Checker(Resource):
-    def get(self):
-        password = request.headers.get("password")
 
-        token = llm_access_token.get_llm_access_token(password)
-        if token is None:
-            return response_generator.fail("password 无效")
+class LLM_Access_Handler(Resource):
+    def get(self, call_type):
+        #get token
+        if call_type == 'token':
+            password = request.headers.get("password")
 
-        return response_generator.ok({"token":token})
+            token = llm_access_token.get_llm_access_token(password)
+            if token is None:
+                return response_generator.fail("password 无效")
 
-class LLM_Caller(Resource):
-    def get(self):
+            return response_generator.ok({"token": token})
+
+
         token = request.headers.get("token")
         if llm_access_token.check_llm_access_token(token) is None:
             return response_generator.fail("Token 无效", code=-1)
 
-        data = request.args
-        conv_id = data.get("id")
+        if call_type == 'conversation':
+            data = request.args
+            conv_id = data.get("id")
 
-        result = llm_middleware.get_conversation(conv_id)
-        if result is None:
-            return response_generator.fail("无对话", code=-2)
+            result = llm_middleware.get_conversation(conv_id)
+            if result is None:
+                return response_generator.fail("无对话", code=-2)
 
-        return response_generator.ok(result.to_json())
+            return response_generator.ok(result.to_json())
 
-    def post(self):
+        return response_generator.fail("unknown call type", code=-10)
+
+
+    def post(self, call_type):
         token = request.headers.get("token")
         if llm_access_token.check_llm_access_token(token) is None:
             return response_generator.fail("Token 无效", code=-1)
 
         data = request.get_json()
-        call_type = data.get("type")
-        collection_name = data.get("collection_name")
         if call_type == 'new':
             text = data.get("text")
+            collection_name = data.get("collection_name")
             conv = llm_middleware.Conversation(llm_middleware.new_conversation(collection_name, text))
             conv.requests(text)
             conv.update_context()
             return response_generator.ok(conv.to_json())
         elif call_type == 'top_ask':
+            collection_name = data.get("collection_name")
             top_asks = [x.to_json() for x in llm_middleware.top_ask(collection_name)]
             return response_generator.ok(top_asks)
         elif call_type == 'top_close':
             text = data.get("text")
+            collection_name = data.get("collection_name")
             top_closes = [x.to_json() for x in llm_middleware.question_lookup(collection_name,text)]
             for c in top_closes: c.add_frequency()
             return response_generator.ok(top_closes)
@@ -83,14 +90,14 @@ class LLM_Caller(Resource):
         elif call_type == 'dislike':
             conv_id = data.get("id")
             return response_generator.ok(llm_middleware.dislike(conv_id))
-        else:
-            return response_generator.fail("type 无效", code=-2)
+
+
+        return response_generator.fail("unknown call type", code=-10)
 
 
 
 api.add_resource(Hello, '/hello')
-api.add_resource(LLM_Caller, '/accllm')
-api.add_resource(LLM_Access_Checker, '/accllm/token')
+api.add_resource(LLM_Access_Handler, '/accllm/<string:call_type>')
 
 
 if __name__ == "__main__":
